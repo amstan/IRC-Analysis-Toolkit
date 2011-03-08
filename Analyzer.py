4521efd07744107fb996f59f11e5b2a604b7128f
Alexandru Stan <amstan@csclub.uwaterloo.ca>
2011-03-07 19:14:25 -0500

    worked more on the analyzer, stuck on highlight regexes

--- Analyzer.py
+++ Analyzer.py
@@ -1,15 +1,64 @@
 #!/usr/bin/env python
 
 from LogReader import *
+import sys
 import re
 
-msgSplitPattern = re.compile(r"<([^>]+)>")
+ACTION, MISC, MSG = range(3)
 
-reader=LogReader("logs/*.log")
+def parseLog(reader):
+	
+	log = []
+	nicks = set()
 
-lines = []
-for line in reader:
-	print line
-	line = re.split(msgSplitPattern, line, maxsplit=2)
-	#lines.append(line)
-	print "%s %s" % (len(line), line)
+	for line in reader:
+		date, _, nick, msg = line.split(' ',3)
+		
+		if nick=="*":
+			linetype = ACTION
+			nick, msg = msg.split(' ',1)
+		elif nick=="***":
+			linetype = MISC
+			nick, msg = msg.split(' ',1)
+		else:
+			linetype = MSG
+			nick = nick[1:-1].rstrip("_")
+		
+		nicks.add(nick)
+		log.append([linetype,date,nick,msg])
+		
+		#print "Parsing logs @ %s" % (date)
+		sys.stdout.flush()
+	
+	return log, nicks
+
+def main():
+	reader=LogReader("logs/*.log")
+	
+	log, nicks = parseLog(reader)
+	
+	graph = {}
+	for nick in nicks:
+		graph[nick] = set()
+	
+	print "Nicks: %s" % (", ".join(nicks))
+	
+	nickre = re.compile("\\b(%s)*\\b" % ("|".join(re.escape(nick.lower()) for nick in nicks)))
+	
+	#Do some tests for the match
+	print nickre.match("xenon: amstan: test").groups()
+	print nickre.match("gfdsgsjhsdfrwagh").groups()
+	
+	#for line in log[:0]:
+		#print line
+		#linetype, date, nick, msg = line
+		#if linetype == MSG:
+			#line=line.lower()
+			#if msg.find(possiblenick)!=-1:
+				#print possiblenick
+		
+		#print "Searching for edges @ %s" % (date)
+		#sys.stdout.flush()
+
+if __name__ == "__main__":
+	main()
