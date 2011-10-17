75f41bca70dd64c146da615fb3ffbb2582c5a0f2
Alexandru Stan <alex@hypertriangle.com>
2011-10-16 23:54:59 -0400

    check if serious questions for faq

--- faq.py
+++ faq.py
@@ -10,20 +10,24 @@ def faq(filelist):
 	log, _ = parseLog(reader)
 	
 	faqs={}
+	def addfaq(question):
+		try:
+			faqs[line[3]]+=1
+		except KeyError:
+			faqs[line[3]]=1
 	
 	for line in log:
 		if line[0]==MSG:
+			#check if question
 			if line[3].find("?")>=0:
-				try:
-					faqs[line[3]]+=1
-				except KeyError:
-					faqs[line[3]]=1
+				#check if serious question
+				if len(line[3].split())>2:
+					addfaq(line[3])
 	
-	for q,count in faqs.items():
+	for q,count in sorted(faqs.items(),cmp=lambda x,y: cmp(x[1], y[1])):
 		if count>1:
 			print count, q
 
 if __name__ == "__main__":
 	import sys
 	faq(sys.argv[1:])
-	
